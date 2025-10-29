e1bb3c4d752bf40ab744334b41e56d355d7a3df9
aguer47 <maguer@byupathway.edu>
2025-10-29 11:01:59 +0300

    add index.html

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+const today = new Date();
+
+document.getElementById("currentyear").textContent = today.getFullYear();
+
+
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
